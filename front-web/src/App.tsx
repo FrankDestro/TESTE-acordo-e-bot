0def695b0c58d0fe66fe51f5089989d15dcef9b7
FrankDestro <frank-destro@outlook.com>
2022-12-06 23:56:41 -0300

    Add SalesTable component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/header';
 import PieChartCard from './components/pie-chart-card';
 import SalesByDate from './components/sales-by-date';
 import SalesSummary from './components/sales-summary';
+import SalesTable from './components/sales-table';
 
 function App() {
   return (
@@ -18,8 +19,8 @@ function App() {
          series={[40, 30, 30]} />
           <PieChartCard name='Pagamento' labels={['Credito', 'Debito', 'Dinheiro']}
          series={[20, 50, 30]} />
-       
         </div>
+        <SalesTable></SalesTable>
       </div>
     </>
   );
